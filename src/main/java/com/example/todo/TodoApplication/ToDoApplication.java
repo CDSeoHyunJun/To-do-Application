fc8983bab01c8e0562b775ef0877723125bd2ce7
CDSeoHyunJun <sjmsjm0302@naver.com>
2023-04-12 21:36:27 +0900

    second commit

---
+++ src/main/java/com/example/todo/TodoApplication/ToDoApplication.java
@@ -0,0 +1,13 @@
+package com.example.todo.TodoApplication;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ToDoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ToDoApplication.class, args);
+	}
+
+}
